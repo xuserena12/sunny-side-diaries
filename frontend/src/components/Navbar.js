d8e798c133f94674417bf98ee7518b88ae9a814f
Jessica Chen <jesschen046@gmail.com>
2023-12-21 23:36:51 -0500

    navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,17 @@
 import { Link } from 'react-router-dom';
-
+import './Navbar.css';
 const Navbar = () => {
   return (
-    <nav className="bg-cream">
+    <nav className="navbar">
       <div className="flex justify-end p-4">
         <ul className="flex gap-8 items-center">
           <li>
-            <Link to="/" className="text-brown text-2xl">Home</Link>
+            <Link to="/" className="navbar-link">Home</Link>
           </li>
           <li>
-            <Link to="/myjournal" className="text-brown text-2xl">My Journal</Link>
+            <Link to="/myjournal" className="navbar-link">My Journal</Link>
           </li>
-          <li className="text-brown text-2xl">
-            <div className="inline-block bg-yellow pt-1 pb-1 pr-6 pl-6 rounded-3xl border-2 border-solid border-brown">
-              Log Out
-            </div>
-          </li>
+          <li className="navbar-link"> Log Out</li>
         </ul>
       </div>
     </nav>
