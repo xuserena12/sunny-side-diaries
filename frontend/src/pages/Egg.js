a4763fd82ac3af880719b9d4482cf9d35c65336f
Jessica Chen <jesschen046@gmail.com>
2023-12-23 12:56:08 -0500

    added animations

--- frontend/src/pages/Egg.js
+++ frontend/src/pages/Egg.js
@@ -6,17 +6,20 @@ const Egg = () => {
   console.log('Rendering Egg component');
   return (
     <div className="pt-16 bg-chooseEgg bg-cover w-screen h-screen">
-      <div className="oval"></div>
-        <div className="EggText">Choose Your Egg</div>
+      <div className="oval"></div>      
         <div className = "tamago-container">
+          <h1>Tamago</h1>
           <img src={tamagoPic} alt="tamago"></img>
         </div>
         <div className = "jimmy-container">
+          <h1>Jimmy</h1>
           <img src={jimPic} alt="jim"></img>
         </div>
         <div className = "sunny-container">
+          <h1>Sunny</h1>
           <img src={sunnyPic} alt="sunny"></img>
-  </div>
+        </div>
+        <div className="EggText">Choose Your Egg</div>
     </div>
   );
 }
