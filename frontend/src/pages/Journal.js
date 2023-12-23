7914314e4bdb42526cd45d61d442c53ed67e3365
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-12-22 21:45:16 -0500

    journal page styling

--- frontend/src/pages/Journal.js
+++ frontend/src/pages/Journal.js
@@ -1,7 +1,18 @@
+import './Journal.css';
+
 const Journal = () => {
   return (
-    <div className="pt-16 bg-main bg-cover h-screen w-screen">
-      Journal goes here!
+    <div className="bg-main bg-cover h-screen w-screen flex justify-center items-center">
+      <div className="outer-journal">
+        <div className="inner-journal">
+          <div className="content-wrapper ml-4 mt-4">
+            <textarea
+              className="w-full h-full outline-none resize-none"
+              placeholder="Write your thoughts here..."
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
