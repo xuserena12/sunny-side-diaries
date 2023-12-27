546a77f61e622147e933ba4ec0d8cd13a4eb2bf4
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-12-26 23:01:41 -0500

    redo

--- frontend/src/pages/History.js
+++ frontend/src/pages/History.js
@@ -1,7 +1,27 @@
 import './History.css';
 import Entry from '../components/Entry';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
+
 
 const History = () => {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('http://localhost:8000');
+  //       console.log(response.data);
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+
   return (
     <div className="bg-main bg-cover h-screen w-screen flex justify-center items-center">
       <div className="outer-box">
