5a7f1df99a7efbb066b12658676a786e54909667
Jessica Chen <jesschen046@gmail.com>
2023-12-28 20:05:35 -0500

    sign in

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const indexRouter = require('./routes/index');
 app.use(express.json())
 app.use('/', indexRouter);
 
+const cors = require("cors");
+app.use(cors());
+
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -31,3 +34,19 @@ app.listen(process.env.PORT, () => {
   console.log("Listening on port", process.env.PORT);
 });
 
+require("./userDetails")
+const User=mongoose.model("UserInfo");
+app.post("/register", async(req, res) => {
+  const {fname, lname, email, password}=req.body;
+  try {
+    await User.create({
+      fname,
+      lname,
+      email,
+      password,
+    });
+    res.send({status: "ok"});
+  } catch (error) {
+    res.send({ status: "error"});
+  }
+});
