94c00c75a92571fb4941d89ed088eb27bd191091
Serena X <84420380+xuserena12@users.noreply.github.com>
2023-12-29 13:47:11 -0500

    entry styling

--- frontend/src/components/Entry.js
+++ frontend/src/components/Entry.js
@@ -1,9 +1,21 @@
 import './Entry.css';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const Entry = ({ date, title, content }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/")
+  }
+
   return (
     <div className="outer-entry">
-      <div className="entry">
+      <div
+      className="entry"
+      onClick={handleClick}
+      >
         <h3>{title}</h3>
         <p>Date: {date}</p>
         <p>{content}</p>
