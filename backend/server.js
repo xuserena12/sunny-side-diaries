c24119060e0b3f604f3ec500ce0178b900cc7241
Jessica Chen <jesschen046@gmail.com>
2023-12-31 00:45:41 -0500

    login implementation

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ app.use('/', indexRouter);
 const cors = require("cors");
 app.use(cors());
 
-const bcrypt=require("bcrypt");
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 // Set up mongoose connection
@@ -35,25 +38,46 @@ app.listen(process.env.PORT, () => {
 
 require("./userDetails")
 const User=mongoose.model("UserInfo");
+// sign up API
 app.post("/register", async(req, res) => {
   const {fname, lname, email, password}=req.body;
-  const salt = await bcrypt.genSalt(Number(process.env.SALT));
-  const encryptedPass=await bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(Number(process.env.SALT)); // password encryption variable
+  const encryptedPass=await bcrypt.hash(password, salt); // hash password
   try {
-    const oldUser= await User.findOne({ email });
+    // checking to see if user with email already exists
+    const oldUser= await User.findOne({ email }); 
     if (oldUser) {
        console.log("hi");
        return res.status(409).send({message: "User with given email already exists"});
-    }
-    await User.create({
+    } 
+    await User.create({ // creating a new user in database
       fname,
       lname,
       email,
       password:encryptedPass,
     });
-    res.status(201).send({message: "User created successfully"});
+    res.status(201).send({message: "User created successfully"}); 
   } catch (error) {
     res.status(201).send({message: "Internal Server Error"});
   }
 });
 
+// Login API
+app.post("/login", async(req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user=await User.findOne({email});
+    if (!user) {
+      return res.status(401).send({message: "Invalid Email or Password"});
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({email: user.email}, JWT_SECRET, {expiresIn: "15m"});
+      res.status(200).send({data:token, message: "Logged in successfully"})
+    }
+    return res.status(401).send({message: "Invalid Email or Password"});
+  }
+  catch(error) {
+    return res.status(500).send({message:"Internal Server Error"});
+  }
+
+})
--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,25 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 import './Sign.css';
+import { Link, useNavigate } from "react-router-dom";
+
 const Signin = () => {
+    const [data, setData] = useState({
+        email:"",
+        password:"",
+    });
+    const [error, setError] = useState();
+    const [success, setSuccess] = useState();
+    const navigate = useNavigate();
+
+    const handleChange = ({ currentTarget: input }) => {
+        setData({...data, [input.name]: input.value});
+        setError('');
+    };
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try {
+            const url = "http://localhost:4000/login";
+            const { data: res } = await axios.post(url, data);
+            localStorage.setItem("token", res.data);
+            // window.location = "/";
+            setSuccess(res.message);
+            console.log(res.message);
+            navigate('/journal');
+        }
+        catch(error) {
+            if (
+                error.response &&
+                error.response.status >= 400 && error.response.status <= 500
+            ) {
+                setError(error.response.data.message);
+            }
+        }
+    }
     return (
-        <form className="bg-main background">
+        <form onSubmit={handleSubmit} className="bg-main background">
             <div className="overall">
-        <h3>Sign In</h3>
-        <div className="fillbox">
-            <input type="email" className="form-control" placeholder="Enter Email"></input>
-        </div>
-        <div className="fillbox">
-            <input type="password" className="form-control" placeholder="Enter Password"></input>
-        </div>
-        <div className="d-grid">
-                <button type="submit" className="btn btn-primary">
-                    Sign In
-                </button>
-        </div>
-        <p className="forgot-password underline">
-          Forgot <a href="#">password?</a>
-        </p>
-        </div>
+            <h3>Sign In</h3>
+            <div className="fillbox">
+            <input 
+                    type="email"
+                    placeholder="Email"
+                    name='email'
+                    onChange={handleChange}
+                    value = {data.email}
+                    required
+                    className="form-control"
+                />
+            </div>
+            <div className="fillbox">
+                <input 
+                    type="password"
+                    placeholder="Password"
+                    name='password'
+                    onChange={handleChange}
+                    value = {data.password}
+                    required
+                    className="form-control"
+                />
+            </div>
+            {success && <p className="success-message">{success}</p>}
+            {error && <p className="error-message">{error}</p>}
+            <div className="d-grid">
+                    <button type="submit" className="btn btn-primary">
+                        Sign In
+                    </button>
+            </div>
+            <p className="forgot-password underline">
+            Forgot <a href="#">password?</a>
+            </p>
+            </div>
       </form>
     )
 };
