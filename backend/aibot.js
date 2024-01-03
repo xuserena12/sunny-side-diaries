eb6c94d5bc8c9fc3ce9bc2eed66f9200e1677dbe
Serena X <84420380+xuserena12@users.noreply.github.com>
2024-01-03 00:02:41 -0500

    added sentiment analysis

--- backend/aibot.js
+++ backend/aibot.js
@@ -8,6 +8,7 @@ const { Configuration, OpenAIApi } = require('openai');
 const config = new Configuration({
     apiKey: process.env.AI_API_KEY,
 });
+const text2emotion = require('text2emotion');
 
 const openai = new OpenAIApi(config);
 
@@ -17,6 +18,11 @@ app.use(cors());
 
 app.post("/chat", async(req, res) => {
     const { prompt } = req.body;
+
+    // analyse the emotions here
+    const { entry } = req.body;
+    const emotions = text2emotion.getEmotion(entry);
+
     const completion = await openai.createCompletion({
         model: "gpt-3.5-turbo", 
         max_tokens: 512,
@@ -24,7 +30,6 @@ app.post("/chat", async(req, res) => {
     });
 
     res.send(completion.data.choices[0].text);
-    
 })
 
 
